7afb33e84dd64143fc7deb617a1716ac9aacece5
JiM Strassburg <jstrassburg@gmail.com>
2017-04-15 22:20:29 -0500

    Fixing dropbox to run off of /home/puke/Dropbox

--- dropbox.sh
+++ dropbox.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 NAME=dropbox
-USER=dropbox
 
 docker run -d \
   --name=$NAME \
   --restart=always \
   --net=host \
   -e VERSION=latest \
-  -e DBOX_UID=`id -u $USER` -e DBOX_GID=`id -g $USER` \
-  -v /mnt/data/dropbox:/dbox/Dropbox \
+  -e DBOX_UID=`id -u` -e DBOX_GID=`id -g` \
+  -v $HOME/Dropbox:/dbox/Dropbox \
   janeczku/dropbox
